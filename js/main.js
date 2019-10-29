ef79e1d11fa5e082bcd68083f34c021eb5d64268
anxoret <post.lux@yandex.ru>
2019-10-29 17:50:14 +0300

    Step 2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict" 
+
+// const showAndHideDomElement = (element) => {
+//     element.classList.toggle("date__ul_open");
+//     alert("<D");
+// };
+
+
+// let dates = document.querySelectorAll(".date-name_first-theme");
+// let datesUl = document.querySelectorAll(".date__ul_open");
+
+// dates.forEach( (date, i) => {
+//     date.addEventListener("click", showAndHideDomElement(datesUl[i]));
+// });
