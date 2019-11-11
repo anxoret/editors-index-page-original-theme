5e1ce2018d6df3c1ebe153aead5bd76d5e6eac94
anxoret <post.lux@yandex.ru>
2019-11-11 11:52:48 +0300

    Working before elements for close and open time periods menus

--- js/open-appeals-by-the-time.js
+++ js/open-appeals-by-the-time.js
@@ -57,9 +57,16 @@ arrayOfDatesLi.forEach(dateLi => {
 });
 
 // primary on page Editors index page we show latest date with its latest appeals  
-let allDatesLi = document.querySelectorAll(".date__li");
-let lastDateLi = allDatesLi[allDatesLi.length - 1];
+let allDates = document.querySelectorAll(".date");
+let lastDate = allDates[allDates.length - 1];
+// let allDatesLi = document.querySelectorAll(".date__li");
+// let lastDateLi = allDatesLi[allDatesLi.length - 1];
+let allLastDateLi = lastDate.querySelectorAll(".date__li");
+let lastDateLi = allLastDateLi[allLastDateLi.length - 1];
 openTheDateAppeals(lastDateLi);
+let lastDateName = lastDate.querySelector(".date__name");
+lastDateName.classList.remove("date__name_arrow-right");
+lastDateName.classList.add("date__name_arrow-down");
 
 const getElementAbsoluteHeight = (element) => {
     element = (typeof element === "string") ? document.querySelector(element) : element;
